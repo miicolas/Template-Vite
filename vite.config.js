af204f6ee418c06c7653dc4f29729477e656df7f
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-04-04 21:56:04 +0200

    ✨

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-const isCodeSandbox = !!process.env.SANDBOX_URL
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
-    root: "src/",
-    publicDir: "../static/",
-    base: "./",
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
     server:
     {
         host: true,
-        open: !isCodeSandbox
+        open: !isCodeSandbox // Open if it's not a CodeSandbox
     },
     build:
     {
-        outDir: "../dist",
+        outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true
     }
